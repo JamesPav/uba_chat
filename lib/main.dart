c3dbf4e7354c3b99ed76a890f0c28adc0ada3995
JamesPav <25965588+JamesPav@users.noreply.github.com>
2021-04-20 20:55:28 +0300

    animated welcome screen - firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -3,24 +3,24 @@ import 'package:uba_chat/screens/welcome_screen.dart';
 import 'package:uba_chat/screens/login_screen.dart';
 import 'package:uba_chat/screens/registration_screen.dart';
 import 'package:uba_chat/screens/chat_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() => runApp(FlashChat());
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(FlashChat());
+}
 
 class FlashChat extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.dark().copyWith(
-        textTheme: TextTheme(
-          bodyText2: TextStyle(color: Colors.black54),
-        ),
-      ),
-      initialRoute: "welcome_screen",
+      initialRoute: WelcomeScreen.id,
       routes: {
-        "welcome_screen": (context) => WelcomeScreen(),
-        "login_screen": (context) => LoginScreen(),
-        "registration_screen": (context) => RegistrationScreen(),
-        "chat_screen": (context) => ChatScreen(),
+        WelcomeScreen.id: (context) => WelcomeScreen(),
+        LoginScreen.id: (context) => LoginScreen(),
+        RegistrationScreen.id: (context) => RegistrationScreen(),
+        ChatScreen.id: (context) => ChatScreen(),
       },
     );
   }
